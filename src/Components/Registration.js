ec19c6fe332bb9beaf6733fdee3b090762313404
Almıla Sultan TAŞ <65083331+almilasultantas@users.noreply.github.com>
2024-01-21 01:12:59 +0300

    frontend

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -7,7 +7,7 @@ function Registration() {
   return (
     <Container className="col-md-5 mx-auto">
       <Form >
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
         <Form.Label column sm="3">
           Ad
         </Form.Label>
@@ -15,7 +15,7 @@ function Registration() {
           <Form.Control  placeholder="Ad" />
         </Col>
       </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextSurname">
         <Form.Label column sm="3">
           Soyad
         </Form.Label>
@@ -23,14 +23,7 @@ function Registration() {
           <Form.Control  placeholder="Soyad" />
         </Col>
       </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="3">
-          Kullanıcı Adı
-        </Form.Label>
-        <Col sm="9">
-          <Form.Control  placeholder="Username" />
-        </Col>
-      </Form.Group>
+      
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
         <Form.Label column sm="3">
           Email
