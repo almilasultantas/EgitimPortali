e4cc09ee037bb05579527250aa1efeed0fa2b4fe
Almıla Sultan TAŞ <65083331+almilasultantas@users.noreply.github.com>
2024-01-23 20:30:43 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import Navbar from './Components/Navbar';
+import Registration from './Components/Registration';
 import RouterPage from './Components/RouterPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Registration/>
       <RouterPage/>
     </div>
   );
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -3,46 +3,74 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import axios from 'axios';
 function Registration() {
+
+  const[FistName,setFirstName]=useState("")
+  const[LastName,setLastName]=useState("")
+  const[Email,setEmail]=useState("")
+  const[Password,setPassword]=useState("")
+ 
+
+  const handleSave=(e)=>{
+    e.preventDefault();
+    const url ="https://localhost:7237/api/Users/registration"
+    const data= {
+      FirstName : FistName,
+      LastName: LastName,
+      Email:Email,
+      Password:Password,
+
+    }
+    axios.post(url,data)
+    .then((result)=>{
+      const dt= result.data;
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+
   return (
     <Container className="col-md-5 mx-auto">
       <Form >
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
-        <Form.Label column sm="3">
+        <Form.Label column sm="3" >
           Ad
         </Form.Label>
         <Col sm="9">
-          <Form.Control  placeholder="Ad" />
+          <Form.Control  placeholder="Ad" onChange={(e)=>setFirstName(e.target.value)} />
         </Col>
       </Form.Group>
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextSurname">
-        <Form.Label column sm="3">
+        <Form.Label column sm="3" >
           Soyad
         </Form.Label>
         <Col sm="9">
-          <Form.Control  placeholder="Soyad" />
+          <Form.Control  placeholder="Soyad" onChange={(e)=>setLastName(e.target.value)} />
         </Col>
       </Form.Group>
       
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="3">
+        <Form.Label column sm="3" >
           Email
         </Form.Label>
         <Col sm="9">
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="email" placeholder="name@example.com" onChange={(e)=>setEmail(e.target.value)}/>
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="3">
+        <Form.Label column sm="3" >
           Password
         </Form.Label>
         <Col sm="9">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
         </Col>
       </Form.Group>
     </Form>
-    <Button variant="primary">Kaydol</Button>{' '}
+    <Button variant="primary" onClick={(e)=>handleSave(e)}>Kaydol</Button>{' '}
     </Container>
     
   );
